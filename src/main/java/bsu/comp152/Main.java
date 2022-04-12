23e8933fda9dd031904732abab5fdd62b01f5646
Kush <94019918+kushpatel7764@users.noreply.github.com>
2022-04-12 12:14:18 -0400

    Update Main.java

--- src/main/java/bsu/comp152/Main.java
+++ src/main/java/bsu/comp152/Main.java
@@ -1,69 +1,43 @@
 package bsu.comp152;
 
+import java.util.Random;
+
 public class Main {
-
     public static void main(String[] args) {
-
-        System.out.println("Countdown!");
-        // Initialize the loop variable(s).
-        int number = -1;
-        // It is possible to have a while loop
-        // that is not entered even once.
-        while (number >= 0) {
-            System.out.println(number);
-            // Update a loop variable in the body
-            // of the while.
-            number--;
+        String output = "";
+        Random rand = new Random();
+        double[] a1 = new double[10];
+        for (int i = 0; i < a1.length; i++){
+            a1[i] = rand.nextInt(10) + 1;
         }
-        System.out.println("Blastoff!");
-
-        System.out.println("Countdown!");
-        for (int i = 10; i >= 0; i--) {
-            System.out.println(i);
+        for(int i = 0; i < a1.length; i++){
+            output += a1[i] + " ";
         }
-        System.out.println("Blastoff!");
-
-        System.out.println("Countdown!");
-        // Initialize the loop variable(s).
-        number = 10;
-        // The body of do-while is executed
-        // at least once.
-        do {
-            System.out.println(number);
-            // Update a loop variable in the body
-            // of the do-while.
-            number--;
-        } while (number >= 0);
-
-        System.out.println("Blastoff!");
-
-        int day = 0;
-        switch (day) {
-            case 0:
-                System.out.println("Sun");
-                // What happens in case 0 without break?!
-                // break;
-            case 1:
-                System.out.println("Mon");
-                break;
-            case 2:
-                System.out.println("Tues");
-                break;
-            case 3:
-                System.out.println("Wed");
-                break;
-            case 4:
-                System.out.println("Thurs");
-                break;
-            case 5:
-                System.out.println("Fri");
-                break;
-            case 6:
-                System.out.println("Sat");
-                break;
-            default:
-                System.out.println("Invalid day");
-        }
-
+        System.out.println(output);
+        System.out.println(selectionSort(a1));
     }
+
+    public static String selectionSort(double[] sortArray){
+        int smallIndex = 0;
+        String output = "";
+        for(int i = 0; i < sortArray.length; i++){
+            double minval = sortArray[i];
+            for(int j = 0; j < sortArray.length; j++){
+                if (minval > sortArray[j]){
+                    minval = sortArray[j];
+                    smallIndex = j;
+                }
+            }
+        double holder = sortArray[i];
+            sortArray[i] = minval;
+            sortArray[smallIndex] = holder;
+        }
+        for(int i = 0; i < sortArray.length; i++){
+            output += sortArray[i] + " ";
+        }
+        return output;
+    }
+
+
 }
+

16ebe43b3121c3aa54b0af014502eaacda0f37a5
Kush <94019918+kushpatel7764@users.noreply.github.com>
2022-04-13 21:04:20 -0400

    Update Main.java

--- src/main/java/bsu/comp152/Main.java
+++ src/main/java/bsu/comp152/Main.java
@@ -4,40 +4,48 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        String output = "";
+        String output1 = "";
+        String output2 = "";
         Random rand = new Random();
-        double[] a1 = new double[10];
+        int[] a1 = new int[10];
         for (int i = 0; i < a1.length; i++){
             a1[i] = rand.nextInt(10) + 1;
         }
         for(int i = 0; i < a1.length; i++){
-            output += a1[i] + " ";
+            output1 += a1[i] + " ";
         }
-        System.out.println(output);
-        System.out.println(selectionSort(a1));
+        System.out.println(output1);
+        selectionSort(a1);
+        for(int i = 0; i < a1.length; i++){
+            output2 += a1[i] + " ";
+        }
+        System.out.println(output2);
     }
 
-    public static String selectionSort(double[] sortArray){
-        int smallIndex = 0;
+    public static int[] selectionSort(int[] sortArray){
+        int smallIndex;
+        int smallValue;
         String output = "";
-        for(int i = 0; i < sortArray.length; i++){
-            double minval = sortArray[i];
-            for(int j = 0; j < sortArray.length; j++){
-                if (minval > sortArray[j]){
-                    minval = sortArray[j];
+        for(int i = 0; i < (sortArray.length -1); i++){
+            // Initialization of values
+            smallIndex = i;
+            smallValue = sortArray[i];
+            // The initialized values get compared throughout the rest of the array
+            for(int j = i + 1; j < sortArray.length; j++){
+                if (sortArray[j] < smallValue){
+                    smallValue = sortArray[j];
                     smallIndex = j;
                 }
             }
-        double holder = sortArray[i];
-            sortArray[i] = minval;
-            sortArray[smallIndex] = holder;
+            //Swap happens here
+            sortArray[smallIndex] = sortArray[i];
+            sortArray[i] = smallValue;
         }
-        for(int i = 0; i < sortArray.length; i++){
-            output += sortArray[i] + " ";
-        }
-        return output;
+        // Final array is returned 
+        return sortArray;
     }
 
 
 }
+
 
